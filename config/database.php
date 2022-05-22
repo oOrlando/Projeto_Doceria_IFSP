df672fe2221a983be7f03e48be658c35d274b0ab
orlando <orlando.oliveira@aluno.ifsp.edu.br>
2022-05-21 21:29:23 -0300

    incluindo mongodb docker

--- config/database.php
+++ config/database.php
@@ -63,6 +63,18 @@ return [
             ]) : [],
         ],
 
+        'mongodb' => [
+            'driver'   => 'mongodb',
+            'host'     => env('MONGO_HOST'),
+            'port'     => env('MONGO_PORT'),
+            'database' => env('MONGO_DATABASE'),
+            'username' => env('MONGO_USERNAME'),
+            'password' => env('MONGO_PASSWORD'),
+            'options'  => [
+                'database' => env('MONGO_DATABASE')
+            ]
+        ],
+
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
