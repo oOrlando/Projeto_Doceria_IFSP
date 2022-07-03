fa34f51ff7c0fc7e3c0be35b502ebbb27af670bd
orlando <orlando.oliveira@aluno.ifsp.edu.br>
2022-07-02 22:45:37 -0300

    database atualizado

--- config/database.php
+++ config/database.php
@@ -44,8 +44,8 @@ return [
         ],
 
         'mysql' => [
-            
-            'url' => env('DATABASE_URL'),
+            'driver' => 'mysql',
+            //'url' => env('DATABASE_URL'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
             'database' => env('DB_DATABASE', 'forge'),
